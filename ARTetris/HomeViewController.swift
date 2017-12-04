bf390563587afd3ada9ea6743fad8d7f344dc592
Matthew King <mking@users.noreply.github.com>
2017-12-03 17:02:40 -0800

    Add launch screen

--- ARTetris/HomeViewController.swift
+++ ARTetris/HomeViewController.swift
@@ -11,5 +11,6 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController!.isNavigationBarHidden = true
     }
 }
