dc0701f1bd1f2a7639d4457e1a8480f651be501d
Matthew King <mking@users.noreply.github.com>
2017-12-10 17:32:26 -0800

    Add move buttons

---
+++ ARTetris/TetrisCategories.swift
@@ -0,0 +1,13 @@
+//
+//  TetrisCategories.swift
+//  ARTetris
+//
+//  Created by matt2 on 12/10/17.
+//  Copyright © 2017 Exyte. All rights reserved.
+//
+
+import Foundation
+
+enum TetrisCategories: Int {
+    case arrow = 1
+}
